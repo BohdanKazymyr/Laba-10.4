3f0b8f50cd9454edad3f33695f727eeb360ec205
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-12-17 22:29:57 +0200

    f

---
+++ Laba 10.4/Laba 10.4.cpp
@@ -0,0 +1,77 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+void CreateTXT(char* fname)
+{
+	ofstream fout(fname);
+	char ch;
+	string s;
+	do
+	{
+		cin.get();
+		cin.sync();
+		cout << "enter line: "; getline(cin, s);
+		fout << s << endl;
+		cout << "continue? (y/n): "; cin >> ch;
+	} while (ch == 'y' || ch == 'Y');
+	cout << endl;
+}
+void PrintTXT(char* fname)
+{
+	ifstream fin(fname);
+	string s;
+	while (getline(fin, s))
+	{
+		cout << s << endl;
+	}
+	cout << endl;
+}
+void Calc(char* fname, int &k, int &j, int &o)
+{
+	ifstream fin(fname);
+	string s;
+	k = 0;
+	char z;
+	j = 0;
+	o = 0;
+	cout << "Find : "; cin >> z;
+	while (getline(fin, s))
+	{
+		for (unsigned i = 0; i < s.length(); i++)
+		{
+			if (s[0] == z && s[i + 1] != '_')
+			{
+				k=1;
+			}
+			else if (s[0] == s[s.length()])
+			{
+				j++;
+			}
+			else if (s[i] == '_')
+			{
+				o++;
+			}
+		}
+	}
+	
+}
+int main()
+{
+	char fname[100];
+	cout << "enter file name 1: "; cin >> fname;
+	CreateTXT(fname);
+	PrintTXT(fname);
+	int k = 0;
+	int j = 0;
+	int o = 0;
+	Calc(fname, k, j, o);
+	cout << "Number of entered = "<< k  << endl;
+	cout << "Number of starting end ending with same = " << j + 1 << endl;
+	cout << "Number of emty = " << o << endl;
+	char gname[100]; // ім'я другого файлу
+	cout << "enter file name 2: "; cin >> gname;
+	PrintTXT(gname);
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 10.4/Laba 10.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char fname[100];
+			CreateTXT(fname);
+			PrintTXT(fname);
+			int k = 0;
+			int j = 0;
+			int o = 0;
+			Calc(fname, k, j, o);
+		}
+	};
+}
